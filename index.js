9edc126c6c5c784df19d5bfbf09941c529d2244b
Enyinna <enyinna.tech@gmail.com>
2020-12-18 13:26:25 +0100

    Entry

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/:shortcode', require('./handlers/shortcode'));
+app.get('/:shortcode/stats', require('./handlers/stats'));
+app.post('/shorten', require('./handlers/shorten'));
+app.post('/usecode', require('./handlers/usecode'));
 
 app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`)
+    console.log(`Listening at http://localhost:${port}`);
 });
