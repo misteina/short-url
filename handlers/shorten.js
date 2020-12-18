1cf5b3c29e78d542f98b12a50a6e4b999bb92ebd
Enyinna <enyinna.tech@gmail.com>
2020-12-18 23:13:00 +0100

    Generate short url

--- handlers/shorten.js
+++ handlers/shorten.js
@@ -1,6 +1,7 @@
 module.exports = function (req, res) {
 
     const validUrl = require('valid-url');
+    const port = require('../share/port');
 
     const longURL = req.body.url;
     const customURL = req.body.custom;
@@ -11,53 +12,52 @@ module.exports = function (req, res) {
 
         connection.beginTransaction(err => {
             if (err) { throw err; }
-            connection.query('SELECT next FROM next_code WHERE id = 3 FOR UPDATE', 
+            connection.query('SELECT code6 FROM next_code WHERE id = 1 FOR UPDATE', 
                 function (error, results, fields){
                     if (error) {
                         return connection.rollback(() => {throw error});
                     }
+                    if (results.length === 1){
+                        let address = results[0].code6.split('.');
 
-                    let address = results[0].next.split('.');
-
-                    for (let i = address.length - 1;i >= 0;i--){
-                        if (address[i] >= 61){
-                            address[i] = 0;
-                        } else if (address[i] < 61){
-                            address[i] += 1;
-                            break;
+                        for (let i = address.length - 1; i >= 0; i--) {
+                            if (address[i] >= 61) {
+                                address[i] = 0;
+                            } else if (address[i] < 61) {
+                                address[i] += 1;
+                                break;
+                            }
                         }
-                    }
 
-                    let addressJoin = address.join('.');
-                    if (addressJoin != '0.0.0.0' && addressJoin != '0.0.0.0.0' && addressJoin != '0.0.0.0.0.0'){
-                        
-                        let charArray = [];
-                        const chars = require('../share/chars');
+                        let addressJoin = address.join('.');
+                        if (addressJoin != '0.0.0.0' && addressJoin != '0.0.0.0.0' && addressJoin != '0.0.0.0.0.0') {
 
-                        for (let i = 0; i < splitCode.length; i++) {
-                            charArray.push(chars[splitCode[i]]);
+                            let charArray = [];
+                            const chars = require('../share/chars');
+
+                            for (let i = 0; i < address.length; i++) {
+                                charArray.push(chars[address[i]]);
+                            }
+                            let shortCode = charArray.join('');
+                            if (typeof customURL !== 'undefined') shortCode = customURL + '/' + shortCode;
+
+                            insertUrl(res, port, connection, shortCode, longURL);
+                        } else {
+                            connection.rollback();
+                            res.json({ status: "error", message: "Limit exceeded" });
                         }
-                        let shortCode = charArray.join('');
-                        if (typeof customURL !== 'undefined') shortCode = customURL + '/' + shortCode;
-
-                        connection.query('INSERT INTO urls SET ?', {short: shortCode, long: longURL}, 
+                    } else {
+                        connection.query('INSERT INTO next_code SET ?', {code6: '0.0.0.0.0.0'}, 
                             (error, results, fields) => {
                                 if (error) throw error;
                                 if (results.affectedRows === 1){
-                                    connection.commit(function (err) {
-                                        if (err) {
-                                            return connection.rollback(function () {
-                                                throw err;
-                                            });
-                                        }
-                                        res.json({ status: "success", message: "Your short url is http://localhost:3000/" + shortCode });
-                                    });
+                                    insertUrl(res, port, connection, 'aaaaaa', longURL);
+                                } else {
+                                    connection.rollback();
+                                    res.json({ status: "error", message: "An error was encountered" });
                                 }
                             }
                         );
-                    } else {
-                        connection.rollback();
-                        res.json({status: "error", message: "Limit exceeded"});
                     }
                 }
             );
@@ -65,4 +65,25 @@ module.exports = function (req, res) {
     } else {
         res.json({status: "error", message: "The provided url is invalid"});
     }
+}
+
+function insertUrl(res, port, connection, shortCode, longURL){
+    connection.query('INSERT INTO urls SET ?', { short: shortCode, long: longURL },
+        (error, results, fields) => {
+            if (error) throw error;
+            if (results.affectedRows === 1) {
+                connection.commit(function (err) {
+                    if (err) {
+                        return connection.rollback(function () {
+                            throw err;
+                        });
+                    }
+                    res.json({ status: "success", message: "Your short url is http://localhost:" + port + "/" + shortCode });
+                });
+            } else {
+                res.json({ status: "error", message: "An error was encountered" });
+            }
+            connection.rollback();
+        }
+    );
 }
