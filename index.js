df6130d9f0d14b4a0a8eeeff7ed3ad15ae573f7f
Enyinna <enyinna.tech@gmail.com>
2020-12-18 23:09:32 +0100

    Entry

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+const helmet = require('helmet');
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = require('./share/port');
 
+app.disable('x-powered-by');
+
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/:shortcode', require('./handlers/shortcode'));
+app.get('/:custom/:shortcode', require('./handlers/custom'));
 app.get('/:shortcode/stats', require('./handlers/stats'));
 app.post('/shorten', require('./handlers/shorten'));
 app.post('/usecode', require('./handlers/usecode'));
+app.post('/reset', require('./handlers/reset'));
+app.get('*', require('./handlers/notfound'));
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
