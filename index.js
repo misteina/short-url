47195787d35b2795b44bd9288e98c08874f73834
Enyinna <enyinna.tech@gmail.com>
2020-12-19 20:40:58 +0100

    Fix

--- index.js
+++ index.js
@@ -11,13 +11,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/:shortcode', require('./handlers/shortcode'));
-app.get('/:custom_:shortcode', require('./handlers/custom'));
-app.get('/:shortcode/stats', require('./handlers/stats'));
 app.post('/shorten', require('./handlers/shorten'));
+app.get('/:shortcode', require('./handlers/shortcode'));
+app.get('/:shortcode/stats', require('./handlers/stats'));
+app.get('/:custom/:shortcode', require('./handlers/custom'));
 app.post('/usecode', require('./handlers/usecode'));
 app.post('/dbreset', require('./database/reset'));
-app.get('*', require('./handlers/notfound'));
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
