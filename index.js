2d1386318fec8f8429d7b2da7348611d124811d1
Enyinna <enyinna.tech@gmail.com>
2020-12-19 21:01:44 +0100

    Comments

--- index.js
+++ index.js
@@ -11,11 +11,21 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Shortens a provided long url
 app.post('/shorten', require('./handlers/shorten'));
+
+// Redirects a short code to orginal url
 app.get('/:shortcode', require('./handlers/shortcode'));
+
+// View short code stats
 app.get('/:shortcode/stats', require('./handlers/stats'));
+
+// Redirects a custom short code to original url 
 app.get('/:custom/:shortcode', require('./handlers/custom'));
+
+// Use a provided a short code if available
 app.post('/usecode', require('./handlers/usecode'));
+
 app.post('/dbreset', require('./database/reset'));
 
 app.listen(port, () => {
